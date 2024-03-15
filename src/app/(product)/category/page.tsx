490543e1fd7e44e5993386fb58f93ffdf03b799d
Yeojinny <pdg03092@gmail.com>
2024-03-15 17:57:00 +0900

    Refactor: Add pages folder to components file structure

--- src/app/(product)/category/page.tsx
+++ src/app/(product)/category/page.tsx
@@ -1,6 +1,6 @@
-import CategoryFooter from "@/components/category/categoryFooter";
-import CategoryList from "@/components/category/categoryList";
-import ThemeList from "@/components/category/themeList";
+import CategoryFooter from "@/components/pages/category/categoryFooter";
+import CategoryList from "@/components/pages/category/categoryList";
+import ThemeList from "@/components/pages/category/themeList";
 import FloatingLeft from "@/components/common/FloatingLeft";
 import React from "react";
 
