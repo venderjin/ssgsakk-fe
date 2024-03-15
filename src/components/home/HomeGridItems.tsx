a2648307702410719628883cf5ae97d7d31ece89
venderjin <lffpz3331@gmail.com>
2024-03-15 11:31:22 +0900

    Refactor: category list gradient border modify

---
+++ src/components/home/HomeGridItems.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomeGridItems = () => {
+    return (
+        <div className="bg-yellow-100">
+            <div>HomeGridItems</div>
+        </div>
+    );
+};
+
+export default HomeGridItems;
