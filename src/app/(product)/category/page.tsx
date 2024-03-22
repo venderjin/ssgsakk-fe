def17dfa54041f773fe691d5d52bdc311323b3a5
Yeojinny <pdg03092@gmail.com>
2024-03-22 18:31:46 +0900

    Fix: fix git switch error

--- src/app/(product)/category/page.tsx
+++ src/app/(product)/category/page.tsx
@@ -1,15 +1,9 @@
-import CategoryFooter from "@/components/pages/category/categoryFooter";
-import CategoryList from "@/components/pages/category/categoryList";
-import ThemeList from "@/components/pages/category/themeList";
 import FloatingLeft from "@/components/common/FloatingLeft";
 import React from "react";
 
 const CategoryPage = () => {
   return (
     <div>
-      <CategoryList />
-      <ThemeList />
-      <CategoryFooter />
       <FloatingLeft />
     </div>
   );
