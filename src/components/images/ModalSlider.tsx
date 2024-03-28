83399e86ea18de405bf314f945c883e4a69082c7
venderjin <lffpz3331@gmail.com>
2024-03-28 09:29:10 +0900

    Feature: slider modal component create

---
+++ src/components/images/ModalSlider.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ModalSlider = () => {
+    return (
+        <div className="w-full mt-2 flex justify-center">
+            <svg width="40" height="4" viewBox="0 0 40 4" fill="none">
+                <rect width="40" height="4" rx="2" fill="#E5E5E5" />
+            </svg>
+        </div>
+    );
+};
+
+export default ModalSlider;
