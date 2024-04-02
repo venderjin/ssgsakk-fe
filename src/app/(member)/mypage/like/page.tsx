bc91c92506d6354cb2b872edcded174408ab6e4c
venderjin <lffpz3331@gmail.com>
2024-04-02 12:09:01 +0900

    Create: like page rounting

---
+++ src/app/(member)/mypage/like/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import BottomNav from "@/components/layouts/BottomNav";
+
+const MyLikePage = () => {
+    return (
+        <>
+            <div>MyLikePage</div>
+            <BottomNav />
+        </>
+    );
+};
+
+export default MyLikePage;
--- src/app/(member)/mypage/page.tsx
+++ src/app/(member)/mypage/page.tsx
@@ -4,18 +4,22 @@ import MyPoint from "@/components/pages/mypage/MyPoint";
 import MyReviewSummary from "@/components/pages/mypage/MyReviewSummary";
 import QuickMenu from "@/components/pages/mypage/QuickMenu";
 import BackArrowHeader from "@/components/common/BackArrowHeader";
+import BottomNav from "@/components/layouts/BottomNav";
 
 const Page = () => {
-  return (
-    <div className="font-Pretendard">
-      <BackArrowHeader title="MY SSG" />
-      <MyPoint />
-      <MyOrderSummary />
-      <QuickMenu />
-      <MyReviewSummary />
-      <MyMenuGroup />
-    </div>
-  );
+    return (
+        <>
+            <div className="font-Pretendard">
+                <BackArrowHeader title="MY SSG" />
+                <MyPoint />
+                <MyOrderSummary />
+                <QuickMenu />
+                <MyReviewSummary />
+                <MyMenuGroup />
+            </div>
+            <BottomNav />
+        </>
+    );
 };
 
 export default Page;
