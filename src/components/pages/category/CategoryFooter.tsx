ad4621caf50a2e08ff1a8b2e9c5f23c94f8e0638
Yeojinny <pdg03092@gmail.com>
2024-04-02 17:59:29 +0900

    Refactor: modify authProvider and category footer for auth

--- src/components/pages/category/CategoryFooter.tsx
+++ src/components/pages/category/CategoryFooter.tsx
@@ -1,27 +1,26 @@
+"use client";
 import { footerData, footerLoginData } from "@/libs/categoryData";
 import Link from "next/link";
-import { getServerSession } from "next-auth";
-import { options } from "@/app/api/auth/[...nextauth]/options";
+import { useSession, signOut } from "next-auth/react";
 
-export default async function CategoryFooter() {
-  const session = await getServerSession(options);
+export default function CategoryFooter() {
+  const { data: session } = useSession();
 
   return (
-    <div className="bg-[#555] pt-[30px] pb-[170px] text-center">
+    <div className="bg-[#555] pt-[30px] pb-[170px] text-center flex justify-center">
       {session
         ? footerLoginData.map((data, index) => (
-            <div key={data.id}>
+            <div
+              key={data.id}
+              className={`pr-[10px] font-Pretendard text-[12px] text-[#cccccc] align-middle ${
+                !index ? "" : "vetical-line"
+              } `}
+            >
               {data.title === "로그아웃" ? (
-                <button>{data.title}</button>
+                <button onClick={() => signOut()}>{data.title}</button>
               ) : (
                 <Link href={data.url}>
-                  <span
-                    className={`pr-[10px] font-Pretendard text-[12px] text-[#cccccc] align-middle ${
-                      !index ? "" : "vetical-line"
-                    } `}
-                  >
-                    {data.title}
-                  </span>
+                  <span>{data.title}</span>
                 </Link>
               )}
             </div>
--- src/components/provider/AuthProvider.tsx
+++ src/components/provider/AuthProvider.tsx
@@ -4,10 +4,8 @@ import { Session } from "next-auth";
 
 export default function AuthProvider({
   children,
-  session,
 }: {
   children: React.ReactNode;
-  session: Session;
 }) {
-  return <SessionProvider session={session}>{children}</SessionProvider>;
+  return <SessionProvider>{children}</SessionProvider>;
 }
