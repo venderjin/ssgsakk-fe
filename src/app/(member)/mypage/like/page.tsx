d593e9710c5ee1ee6dba76fe4e24ad3062074851
venderjin <lffpz3331@gmail.com>
2024-04-08 17:13:59 +0900

    Feat: Create product and category liked and unliked

--- src/app/(member)/mypage/like/page.tsx
+++ src/app/(member)/mypage/like/page.tsx
@@ -3,8 +3,22 @@ import BottomNav from "@/components/layouts/BottomNav";
 import FloatingUp from "@/components/UI/FloatingUp";
 import FloatingLeft from "@/components/UI/FloatingLeft";
 import LikeHeader from "@/components/pages/mypage/like/LikeHeader";
+import { useGetServerToken } from "@/actions/useGetServerToken";
 
-const MyLikePage = () => {
+async function getLikedProductList(token: string) {
+    const response = await fetch(`${process.env.BASE_URL}/likes/user/product`, {
+        headers: {
+            Authorization: token,
+        },
+    });
+    const data = await response.json();
+    // console.log(data.result);
+    return data.result;
+}
+
+const MyLikePage = async () => {
+    const token = await useGetServerToken();
+    const likedProductList = await getLikedProductList(token);
     return (
         <>
             <BackArrowHeader title="좋아요" />
--- src/components/UI/HeartIcon.tsx
+++ src/components/UI/HeartIcon.tsx
@@ -1,29 +1,132 @@
 "use client";
-import React, { useState } from "react";
+import React, { use, useEffect, useState } from "react";
 import RedHeart from "../images/RedHeart";
 import Heart from "../images/Heart";
+import { useGetClientToken } from "@/actions/useGetClientToken";
 
 interface Props {
-  productSeq?: number;
-  height?: number;
-  width?: number;
+    productSeq?: number;
+    categorySeq?: number;
+    height?: number;
+    width?: number;
 }
 
-export default function HeartIcon({ productSeq, height, width }: Props) {
-  const [isCheck, setIsCheck] = useState<Boolean>(false);
+export default function HeartIcon({ productSeq, categorySeq, height, width }: Props) {
+    const [isCheck, setIsCheck] = useState<Boolean | undefined>(undefined);
+    const token = useGetClientToken();
 
-  // const handler = () => {
-  //     setIsCheck(!isCheck);
-  //     handleLike();
-  // };
+    const handler = () => {
+        setIsCheck(!isCheck);
+    };
 
-  return (
-    <div>
-      {isCheck ? (
-        <RedHeart height={height} width={width} />
-      ) : (
-        <Heart height={height} width={width} />
-      )}
-    </div>
-  );
+    useEffect(() => {
+        //랜더링 되었을때 찜 상태인지 아닌지 확인하기 위한 fetch
+        if (productSeq !== undefined && categorySeq === undefined) {
+            const fetchCheckProduct = async () => {
+                const response = await fetch(`${process.env.BASE_URL}/likes/check/product-seq/${productSeq}`, {
+                    method: "GET",
+                    headers: {
+                        Authorization: token,
+                    },
+                });
+                const data = await response.json();
+                // console.log("fetchCheckProduct data", data.result.likeState);
+                setIsCheck(data.result.likeState === 1 ? true : false);
+            };
+            fetchCheckProduct();
+        } else if (productSeq === undefined && categorySeq !== undefined) {
+            const fetchCheckCategory = async () => {
+                const response = await fetch(`${process.env.BASE_URL}/likes/check/category-seq/${categorySeq}`, {
+                    method: "GET",
+                    headers: {
+                        Authorization: token,
+                    },
+                });
+                const data = await response.json();
+                // console.log("fetchCheckCategory data", data.result.likeState);
+                setIsCheck(data.result.likeState === 1 ? true : false);
+            };
+            fetchCheckCategory();
+        }
+    }, []);
+
+    useEffect(() => {
+        //상품 찜 하기
+        if (isCheck === true) {
+            if (productSeq !== undefined && categorySeq === undefined) {
+                // productSeq에 좋아요를 누른 경우 상품 좋아요 api에 fetch
+                const fetchLikeProduct = async () => {
+                    const response = await fetch(`${process.env.BASE_URL}/likes/add?product-seq=${productSeq}`, {
+                        method: "GET",
+                        headers: {
+                            Authorization: token,
+                        },
+                    });
+                    const data = await response.json();
+                    // console.log(data);
+                };
+                fetchLikeProduct();
+            } else if (productSeq === undefined && categorySeq !== undefined) {
+                // categorySeq에 좋아요를 누른 경우
+                const fetchLikeCategory = async () => {
+                    const response = await fetch(`${process.env.BASE_URL}/likes/add?category-seq=${categorySeq}`, {
+                        method: "GET",
+                        headers: {
+                            Authorization: token,
+                        },
+                    });
+                    const data = await response.json();
+                    // console.log(data);
+                };
+                fetchLikeCategory();
+            }
+        }
+
+        //상품 찜 취소
+        if (isCheck === false) {
+            if (productSeq !== undefined && categorySeq === undefined) {
+                // productSeq에 좋아요를 취소한 경우 상품 좋아요 취소 api에 fetch
+                const fetchUnlikeProduct = async () => {
+                    const response = await fetch(`${process.env.BASE_URL}/likes/delete?product-seq=${productSeq}`, {
+                        method: "GET",
+                        headers: {
+                            Authorization: token,
+                        },
+                    });
+                    const data = await response.json();
+                    // console.log("fetchUnlikeProduct data", data);
+                };
+                fetchUnlikeProduct();
+            } else if (productSeq === undefined && categorySeq !== undefined) {
+                // categorySeq에 좋아요를 취소한 경우
+                const fetchUnlikeCategory = async () => {
+                    const response = await fetch(`${process.env.BASE_URL}/likes/delete?category-seq=${categorySeq}`, {
+                        method: "GET",
+                        headers: {
+                            Authorization: token,
+                        },
+                    });
+                    const data = await response.json();
+                    // console.log("fetchUnlikeCategory data", data);
+                };
+                fetchUnlikeCategory();
+            }
+        }
+    }, [isCheck]);
+
+    return (
+        <div onClick={handler}>
+            {isCheck === true ? (
+                <div
+                    style={{
+                        animation: "smallScale 0.5s ease-in-out",
+                    }}
+                >
+                    <RedHeart height={height} width={width} />
+                </div>
+            ) : isCheck === false ? (
+                <Heart height={height} width={width} />
+            ) : null}
+        </div>
+    );
 }
