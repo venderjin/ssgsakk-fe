3ace833e293d564d98819fef0515e18213f7bb2d
venderjin <lffpz3331@gmail.com>
2024-04-08 22:01:26 +0900

    Refactor: topheader component modify to CSR and add CartIcon components

---
+++ src/components/pages/home/HomeSearch.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React, { Suspense } from "react";
+import SearchFrom from "@/components/forms/SearchFrom";
+
+const HomeSearch = () => {
+    return (
+        <>
+            <Suspense>
+                <SearchFrom />
+            </Suspense>
+        </>
+    );
+};
+
+export default HomeSearch;
