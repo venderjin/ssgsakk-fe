456da8cc1d8eeefb4b8492f44354cd0259391a83
venderjin <lffpz3331@gmail.com>
2024-04-10 21:59:35 +0900

    Refactor: custom folder name truncate

--- src/components/pages/mypage/like/LikeFolder.tsx
+++ src/components/pages/mypage/like/LikeFolder.tsx
@@ -12,7 +12,7 @@ const LikeTotalFoler = ({ folderName }: LikeTotalFolerProps) => {
                 <div className="rounded-full bg-[#e4e4e4] w-[57px] h-[57px] flex justify-center items-center">
                     <FilledHeart />
                 </div>
-                <div className="font-Pretendard text-[12px] text-center">{folderName}</div>
+                <div className="font-Pretendard text-[12px] text-center truncate">{folderName}</div>
             </div>
         </>
     );
