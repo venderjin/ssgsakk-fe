bd0ed559bef834f6bfea8f61c6f0bc41e4c3549c
venderjin <lffpz3331@gmail.com>
2024-04-15 17:25:47 +0900

    Refactor: delete annotation from components/pages/login and components/pages/order and components/pages/mypage

--- src/components/pages/mypage/shippingList/ShippingForm.tsx
+++ src/components/pages/mypage/shippingList/ShippingForm.tsx
@@ -1,7 +1,7 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import BackArrowHeader from "@/components/common/BackArrowHeader";
-import { useForm, SubmitHandler, set } from "react-hook-form";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import ShippingFormTextField from "@/components/pages/mypage/shippingList/ShippingFormTextField";
@@ -14,247 +14,220 @@ import { useRouter } from "next/navigation";
 import useRevalidateTag from "@/actions/useRevalidateTag";
 
 interface ShippingFormData {
-  addressNickname: string;
-  receiverName: string;
-  receiverMobileNum: string;
-  zipCode: string;
+    addressNickname: string;
+    receiverName: string;
+    receiverMobileNum: string;
+    zipCode: string;
 }
 
 interface AddressInfoData {
-  zipCode: string;
-  roadAddress?: string;
-  jibunAddress?: string;
-  detailAddress?: string;
+    zipCode: string;
+    roadAddress?: string;
+    jibunAddress?: string;
+    detailAddress?: string;
 }
 
 const schema = yup.object().shape({
-  addressNickname: yup.string().required("주소별칭을 입력해주세요"),
-  receiverName: yup.string().required("받는 분을 입력해주세요"),
-  receiverMobileNum: yup
-    .string()
-    .required("휴대폰 번호를 입력해주세요")
-    .matches(regex.phone, "'-' 제외 11자리를 입력해주세요"),
-  zipCode: yup.string().required("주소를 입력해주세요"),
+    addressNickname: yup.string().required("주소별칭을 입력해주세요"),
+    receiverName: yup.string().required("받는 분을 입력해주세요"),
+    receiverMobileNum: yup.string().required("휴대폰 번호를 입력해주세요").matches(regex.phone, "'-' 제외 11자리를 입력해주세요"),
+    zipCode: yup.string().required("주소를 입력해주세요"),
 });
 
-const ShippingForm = ({
-  shippingData,
-  callbackUrl,
-}: {
-  shippingData: ShippingInfoType;
-  callbackUrl: string | null;
-}) => {
-  const router = useRouter();
-  const { data: session } = useSession();
-  const [openAddress, setOpenAddress] = useState<boolean>(false);
-  const [addressData, setAddressData] = useState<AddressInfoData>({
-    zipCode: "",
-    roadAddress: "",
-    jibunAddress: "",
-    detailAddress: "",
-  });
-
-  const {
-    register,
-    handleSubmit,
-    setValue,
-    getValues,
-    formState: { errors },
-  } = useForm<ShippingFormData>({
-    mode: "onChange",
-    resolver: yupResolver(schema),
-    defaultValues: async () => {
-      if (shippingData) {
-        setAddressData({
-          zipCode: shippingData.zipCode,
-          roadAddress: shippingData.roadAddress,
-          jibunAddress: shippingData.jibunAddress,
-          detailAddress: shippingData.detailAddress,
-        });
-        return {
-          addressNickname: shippingData.addressNickname,
-          receiverName: shippingData.receiverName,
-          receiverMobileNum: shippingData.receiverMobileNum,
-          zipCode: shippingData.zipCode,
-        };
-      } else {
-        return {
-          addressNickname: "",
-          receiverName: "",
-          receiverMobileNum: "",
-          zipCode: "",
-        };
-      }
-    },
-  });
-
-  const resetHandler = () => {
-    setValue("addressNickname", "");
-    setValue("receiverName", "");
-    setValue("receiverMobileNum", "");
-    setValue("zipCode", "");
-    setAddressData({
-      zipCode: "",
-      roadAddress: "",
-      jibunAddress: "",
-      detailAddress: "",
+const ShippingForm = ({ shippingData, callbackUrl }: { shippingData: ShippingInfoType; callbackUrl: string | null }) => {
+    const router = useRouter();
+    const { data: session } = useSession();
+    const [openAddress, setOpenAddress] = useState<boolean>(false);
+    const [addressData, setAddressData] = useState<AddressInfoData>({
+        zipCode: "",
+        roadAddress: "",
+        jibunAddress: "",
+        detailAddress: "",
     });
-  };
 
-  const onChangeAddress = (data: AddressInfoData) => {
-    setValue("zipCode", data.zipCode);
-    setAddressData(data);
-  };
+    const {
+        register,
+        handleSubmit,
+        setValue,
+        getValues,
+        formState: { errors },
+    } = useForm<ShippingFormData>({
+        mode: "onChange",
+        resolver: yupResolver(schema),
+        defaultValues: async () => {
+            if (shippingData) {
+                setAddressData({
+                    zipCode: shippingData.zipCode,
+                    roadAddress: shippingData.roadAddress,
+                    jibunAddress: shippingData.jibunAddress,
+                    detailAddress: shippingData.detailAddress,
+                });
+                return {
+                    addressNickname: shippingData.addressNickname,
+                    receiverName: shippingData.receiverName,
+                    receiverMobileNum: shippingData.receiverMobileNum,
+                    zipCode: shippingData.zipCode,
+                };
+            } else {
+                return {
+                    addressNickname: "",
+                    receiverName: "",
+                    receiverMobileNum: "",
+                    zipCode: "",
+                };
+            }
+        },
+    });
 
-  const onSubmit: SubmitHandler<ShippingFormData> = async (data) => {
-    const updateData = {
-      ...shippingData,
-      addressNickname: data.addressNickname,
-      receiverName: data.receiverName,
-      receiverMobileNum: data.receiverMobileNum,
-      zipCode: data.zipCode,
-      roadAddress: addressData.roadAddress,
-      jibunAddress: addressData.jibunAddress,
-      detailAddress: addressData.detailAddress,
+    const resetHandler = () => {
+        setValue("addressNickname", "");
+        setValue("receiverName", "");
+        setValue("receiverMobileNum", "");
+        setValue("zipCode", "");
+        setAddressData({
+            zipCode: "",
+            roadAddress: "",
+            jibunAddress: "",
+            detailAddress: "",
+        });
     };
 
-    //수정
-    if (shippingData) {
-      const UpdateShippingData = async () => {
-        const res = await fetch(
-          `${process.env.BASE_URL}/shipping-addr/${shippingData.shippingAddressSeq}`,
-          {
-            method: "PUT",
-            headers: {
-              Authorization: session?.user?.token || "",
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(updateData),
-          }
-        );
-        const response = await res.json();
-        useRevalidateTag("address");
-        if (res.ok) {
-          alert("배송지가 수정되었습니다.");
-          if (callbackUrl && callbackUrl !== "undefined")
-            router.push(callbackUrl);
-          else router.push("/mypage/shippingList");
+    const onChangeAddress = (data: AddressInfoData) => {
+        setValue("zipCode", data.zipCode);
+        setAddressData(data);
+    };
+
+    const onSubmit: SubmitHandler<ShippingFormData> = async (data) => {
+        const updateData = {
+            ...shippingData,
+            addressNickname: data.addressNickname,
+            receiverName: data.receiverName,
+            receiverMobileNum: data.receiverMobileNum,
+            zipCode: data.zipCode,
+            roadAddress: addressData.roadAddress,
+            jibunAddress: addressData.jibunAddress,
+            detailAddress: addressData.detailAddress,
+        };
+
+        //수정
+        if (shippingData) {
+            const UpdateShippingData = async () => {
+                const res = await fetch(`${process.env.BASE_URL}/shipping-addr/${shippingData.shippingAddressSeq}`, {
+                    method: "PUT",
+                    headers: {
+                        Authorization: session?.user?.token || "",
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(updateData),
+                });
+                const response = await res.json();
+                useRevalidateTag("address");
+                if (res.ok) {
+                    alert("배송지가 수정되었습니다.");
+                    if (callbackUrl && callbackUrl !== "undefined") router.push(callbackUrl);
+                    else router.push("/mypage/shippingList");
+                } else {
+                    console.log(response.message);
+                    alert("배송지 수정에 실패했습니다");
+                }
+            };
+
+            UpdateShippingData();
         } else {
-          console.log(response.message);
-          alert("배송지 수정에 실패했습니다");
+            //등록
+            const AddShippingData = async () => {
+                const res = await fetch(`${process.env.BASE_URL}/shipping-addr`, {
+                    method: "POST",
+                    headers: {
+                        Authorization: session?.user?.token || "",
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(updateData),
+                });
+                const response = await res.json();
+                useRevalidateTag("address");
+                if (res.ok) {
+                    alert("배송지가 등록되었습니다.");
+                    if (callbackUrl && callbackUrl !== "undefined") router.push(`${callbackUrl}?isModalOpen=true`);
+                    else router.push("/mypage/shippingList");
+                } else {
+                    console.log(response.message);
+                    alert("배송지 등록에 실패했습니다.");
+                }
+            };
+
+            AddShippingData();
         }
-      };
+    };
 
-      UpdateShippingData();
-    } else {
-      //등록
-      const AddShippingData = async () => {
-        const res = await fetch(`${process.env.BASE_URL}/shipping-addr`, {
-          method: "POST",
-          headers: {
-            Authorization: session?.user?.token || "",
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(updateData),
-        });
-        const response = await res.json();
-        useRevalidateTag("address");
-        if (res.ok) {
-          alert("배송지가 등록되었습니다.");
-          if (callbackUrl && callbackUrl !== "undefined")
-            router.push(`${callbackUrl}?isModalOpen=true`);
-          else router.push("/mypage/shippingList");
-        } else {
-          console.log(response.message);
-          alert("배송지 등록에 실패했습니다.");
-        }
-      };
+    return (
+        <div>
+            <BackArrowHeader title={shippingData ? "배송지 수정" : "배송지 추가"} />
+            {openAddress ? (
+                <SearchZipcode onChangeAddress={onChangeAddress} setOpenAddress={setOpenAddress} />
+            ) : (
+                <form className="px-[12px] pb-[40px] font-Pretendard" onSubmit={handleSubmit(onSubmit)}>
+                    <ShippingFormTextField
+                        name="addressNickname"
+                        text="주소별칭"
+                        placeholder="주소별칭 입력"
+                        register={register}
+                        errorMsg={errors.addressNickname?.message}
+                    />
 
-      AddShippingData();
-    }
-  };
+                    <ShippingFormTextField
+                        name="receiverName"
+                        text="받는 분"
+                        placeholder="받는 분 성함 입력"
+                        register={register}
+                        errorMsg={errors.receiverName?.message}
+                    />
 
-  return (
-    <div>
-      <BackArrowHeader title={shippingData ? "배송지 수정" : "배송지 추가"} />
-      {openAddress ? (
-        <SearchZipcode
-          onChangeAddress={onChangeAddress}
-          setOpenAddress={setOpenAddress}
-        />
-      ) : (
-        <form
-          className="px-[12px] pb-[40px] font-Pretendard"
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <ShippingFormTextField
-            name="addressNickname"
-            text="주소별칭"
-            placeholder="주소별칭 입력"
-            register={register}
-            errorMsg={errors.addressNickname?.message}
-          />
+                    <ShippingFormTextField
+                        name="receiverMobileNum"
+                        text="휴대폰"
+                        placeholder="휴대폰(숫자만 입력)"
+                        register={register}
+                        errorMsg={errors.receiverMobileNum?.message}
+                    />
 
-          <ShippingFormTextField
-            name="receiverName"
-            text="받는 분"
-            placeholder="받는 분 성함 입력"
-            register={register}
-            errorMsg={errors.receiverName?.message}
-          />
+                    <ShippingFormTextField
+                        name="zipCode"
+                        text="배송주소"
+                        placeholder=""
+                        register={register}
+                        errorMsg={errors.zipCode?.message}
+                        openAddressModal={() => setOpenAddress(true)}
+                    />
 
-          <ShippingFormTextField
-            name="receiverMobileNum"
-            text="휴대폰"
-            placeholder="휴대폰(숫자만 입력)"
-            register={register}
-            errorMsg={errors.receiverMobileNum?.message}
-          />
+                    {getValues("zipCode") && (
+                        <DetailAddressBox
+                            roadAddress={addressData.roadAddress}
+                            jibunAddress={addressData.jibunAddress}
+                            detailAddress={addressData.detailAddress}
+                        />
+                    )}
 
-          <ShippingFormTextField
-            name="zipCode"
-            text="배송주소"
-            placeholder=""
-            register={register}
-            errorMsg={errors.zipCode?.message}
-            openAddressModal={() => setOpenAddress(true)}
-          />
-
-          {getValues("zipCode") && (
-            <DetailAddressBox
-              roadAddress={addressData.roadAddress}
-              jibunAddress={addressData.jibunAddress}
-              detailAddress={addressData.detailAddress}
-            />
-          )}
-
-          {/* 제출 이벤트 */}
-          <div className="pt-[13px] text-[12px] text-[#666] text-center w-full font-semibold flex gap-1">
-            <button
-              onClick={resetHandler}
-              className="rounded-[2px] border border-[#ccc] bg-[#fff] w-full h-[38px] shadow-inner"
-              type="button"
-            >
-              초기화
-            </button>
-            <button
-              onClick={() => router.push("/mypage/shippingList")}
-              className="rounded-[2px] border border-[#ccc] bg-[#fff] w-full h-[38px] shadow-inner"
-              type="button"
-            >
-              취소
-            </button>
-            <button
-              className="rounded-[2px] border border-[#ccc] bg-[#efefef] w-full h-[38px] shadow-inner"
-              type="submit"
-            >
-              {shippingData ? "수정완료" : "등록"}
-            </button>
-          </div>
-        </form>
-      )}
-    </div>
-  );
+                    {/* 제출 이벤트 */}
+                    <div className="pt-[13px] text-[12px] text-[#666] text-center w-full font-semibold flex gap-1">
+                        <button onClick={resetHandler} className="rounded-[2px] border border-[#ccc] bg-[#fff] w-full h-[38px] shadow-inner" type="button">
+                            초기화
+                        </button>
+                        <button
+                            onClick={() => router.push("/mypage/shippingList")}
+                            className="rounded-[2px] border border-[#ccc] bg-[#fff] w-full h-[38px] shadow-inner"
+                            type="button"
+                        >
+                            취소
+                        </button>
+                        <button className="rounded-[2px] border border-[#ccc] bg-[#efefef] w-full h-[38px] shadow-inner" type="submit">
+                            {shippingData ? "수정완료" : "등록"}
+                        </button>
+                    </div>
+                </form>
+            )}
+        </div>
+    );
 };
 
 export default ShippingForm;
