f9f4968c4b01c64c0eeb13ccb9366dd998288662
Yeojinny <pdg03092@gmail.com>
2024-04-16 20:10:51 +0900

    Refactor: remove unused url

--- src/components/common/SnsButton.tsx
+++ src/components/common/SnsButton.tsx
@@ -1,40 +1,45 @@
 import Imgae from "next/image";
 
 type Props = {
-    snsType: string;
-    snsName: string;
-    iconPosition: string;
+  snsType: string;
+  snsName: string;
+  iconPosition: string;
 };
 
 export default function SnsButton({ snsType, snsName, iconPosition }: Props) {
-    const requestSnsLogin = async (snsType: string) => {
-        window.location.href = `https://ssgssak.site/oauth2/authorization/google`;
-    };
+  const requestSnsLogin = async (snsType: string) => {
+    if (snsType !== "google") {
+      return alert("현재 구글 로그인 서비스만 제공중입니다.");
+    }
+    window.location.href = `https://ssgssak.site/oauth2/authorization/google`;
+  };
 
-    return (
-        <li
-            className="w-[51px] flex flex-col items-center justify-center"
-            onClick={() => {
-                requestSnsLogin(snsType);
-            }}
-        >
-            {snsType === "google" ? (
-                <div className="h-[51px] w-[51px]">
-                    <Imgae
-                        alt="sns 로그인 아이콘"
-                        src="/images/login/google.png"
-                        width={51}
-                        height={51}
-                        className="bg-[#fff] rounded-[50%] w-[51px] h-[51px]"
-                    ></Imgae>
-                </div>
-            ) : (
-                <span
-                    style={{ backgroundPosition: iconPosition }}
-                    className="w-[51px] h-[51px] appearance-none bg-sns-icon bg-no-repeat bg-[length:173px_173px]"
-                />
-            )}
-            <span className="mt-[6px] text-[12px] text-[#4A4A4A] font-Pretendard font-medium">{snsName}</span>
-        </li>
-    );
+  return (
+    <li
+      className="w-[51px] flex flex-col items-center justify-center"
+      onClick={() => {
+        requestSnsLogin(snsType);
+      }}
+    >
+      {snsType === "google" ? (
+        <div className="h-[51px] w-[51px]">
+          <Imgae
+            alt="sns 로그인 아이콘"
+            src="/images/login/google.png"
+            width={51}
+            height={51}
+            className="bg-[#fff] rounded-[50%] w-[51px] h-[51px]"
+          ></Imgae>
+        </div>
+      ) : (
+        <span
+          style={{ backgroundPosition: iconPosition }}
+          className="w-[51px] h-[51px] appearance-none bg-sns-icon bg-no-repeat bg-[length:173px_173px]"
+        />
+      )}
+      <span className="mt-[6px] text-[12px] text-[#4A4A4A] font-Pretendard font-medium">
+        {snsName}
+      </span>
+    </li>
+  );
 }
--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -11,137 +11,181 @@ import { LoginType } from "@/types/authType";
 import Notify from "@/components/UI/Notify";
 import { useAlert } from "@/actions/useAlert";
 
-export default function LoginForm({ callbackUrl, error }: { callbackUrl: string | null; error: string | null }) {
-    const { openAlert } = useAlert();
-    const cookies = getCookies();
-    const [saveIdCheck, setCheck] = useState(false);
-    const [loginData, setLoginData] = useState<LoginType>({
-        loginId: "",
-        password: "",
+export default function LoginForm({
+  callbackUrl,
+  error,
+}: {
+  callbackUrl: string | null;
+  error: string | null;
+}) {
+  const { openAlert } = useAlert();
+  const cookies = getCookies();
+  const [saveIdCheck, setCheck] = useState(false);
+  const [loginData, setLoginData] = useState<LoginType>({
+    loginId: "",
+    password: "",
+  });
+
+  useEffect(() => {
+    if (cookies.rememberUserId) {
+      setLoginData({
+        ...loginData,
+        loginId: cookies.rememberUserId,
+      });
+      setCheck(true);
+    }
+  }, []);
+
+  const onInputCheckboxChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setCheck(event.target.checked);
+    //쿠키 제거
+    if (!event.target.checked) deleteCookie("rememberUserId");
+  };
+
+  const loginSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    //로그인 버튼을 누를 때 쿠키에 저장
+    if (saveIdCheck)
+      setCookie("rememberUserId", loginData.loginId, { path: "/" });
+
+    if (!loginData.loginId || !loginData.password) {
+      const alertData = {
+        isOpen: true,
+        content: "아이디와 비밀번호를 입력해주세요.",
+      };
+      openAlert(alertData);
+      return;
+    }
+
+    const { loginId, password } = loginData;
+    const response = await signIn("user-credentials", {
+      loginId,
+      password,
+      //로그인 실패 시 새로고침 여부
+      redirect: true,
+      callbackUrl:
+        callbackUrl && callbackUrl !== "undefined"
+          ? `${callbackUrl}`
+          : "/mypage",
     });
+  };
 
-    useEffect(() => {
-        if (cookies.rememberUserId) {
-            setLoginData({
-                ...loginData,
-                loginId: cookies.rememberUserId,
-            });
-            setCheck(true);
-        }
-    }, []);
+  const onChangeLoginData = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLoginData({
+      ...loginData,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    const onInputCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setCheck(event.target.checked);
-        //쿠키 제거
-        if (!event.target.checked) deleteCookie("rememberUserId");
-    };
+  return (
+    <div className="p-[20px] pt-[40px]">
+      {error && error !== "undefined" && (
+        <Notify message="아이디 혹은 패스워드가 틀립니다." />
+      )}
+      <form onSubmit={loginSubmit}>
+        <input
+          className="h-[48.5px] w-full border-[#BCBCBC] border-[1px] px-[15px] py-[12px] text-[15px] font-Pretendard"
+          placeholder="아이디"
+          type="text"
+          name="loginId"
+          value={loginData.loginId}
+          onChange={onChangeLoginData}
+        />
+        <input
+          className="h-[48.5px] w-full border-[#BCBCBC] border-[1px] border-t-0 px-[15px] py-[12px] text-[15px] font-Pretendard"
+          placeholder="비밀번호"
+          type="password"
+          name="password"
+          value={loginData.password}
+          onChange={onChangeLoginData}
+        />
 
-    const loginSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+        <div className="flex w-full mt-[10px] px-[15px]">
+          <LoginCheckbox
+            id="keep_id"
+            isChecked={saveIdCheck}
+            labelName="아이디 저장"
+            onChangeCheckbox={onInputCheckboxChange}
+            iconSize={22}
+          />
+        </div>
 
-        //로그인 버튼을 누를 때 쿠키에 저장
-        if (saveIdCheck) setCookie("rememberUserId", loginData.loginId, { path: "/" });
+        <button
+          type="submit"
+          className="base-btn bg-[#FF5452] text-white mt-[30px] font-medium"
+        >
+          로그인
+        </button>
+      </form>
 
-        if (!loginData.loginId || !loginData.password) {
-            const alertData = {
-                isOpen: true,
-                content: "아이디와 비밀번호를 입력해주세요.",
-            };
-            openAlert(alertData);
-            return;
-        }
-
-        const { loginId, password } = loginData;
-        const response = await signIn("user-credentials", {
-            loginId,
-            password,
-            //로그인 실패 시 새로고침 여부
-            redirect: true,
-            callbackUrl: callbackUrl && callbackUrl !== "undefined" ? `${callbackUrl}` : "/mypage",
-        });
-    };
-
-    const onChangeLoginData = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setLoginData({
-            ...loginData,
-            [e.target.name]: e.target.value,
-        });
-    };
-
-    return (
-        <div className="p-[20px] pt-[40px]">
-            {error && error !== "undefined" && <Notify message="아이디 혹은 패스워드가 틀립니다." />}
-            <form onSubmit={loginSubmit}>
-                <input
-                    className="h-[48.5px] w-full border-[#BCBCBC] border-[1px] px-[15px] py-[12px] text-[15px] font-Pretendard"
-                    placeholder="아이디"
-                    type="text"
-                    name="loginId"
-                    value={loginData.loginId}
-                    onChange={onChangeLoginData}
-                />
-                <input
-                    className="h-[48.5px] w-full border-[#BCBCBC] border-[1px] border-t-0 px-[15px] py-[12px] text-[15px] font-Pretendard"
-                    placeholder="비밀번호"
-                    type="password"
-                    name="password"
-                    value={loginData.password}
-                    onChange={onChangeLoginData}
-                />
-
-                <div className="flex w-full mt-[10px] px-[15px]">
-                    <LoginCheckbox id="keep_id" isChecked={saveIdCheck} labelName="아이디 저장" onChangeCheckbox={onInputCheckboxChange} iconSize={22} />
-                </div>
-
-                <button type="submit" className="base-btn bg-[#FF5452] text-white mt-[30px] font-medium">
-                    로그인
-                </button>
-            </form>
-
-            <div className="mt-[13px] w-full h-[19.5px] flex justify-center items-center">
-                {loginSupportData.map((item) => {
-                    return (
-                        <div key={item.id}>
-                            {item.id === 3 ? (
-                                <Link
-                                    className={`px-[7px] text-[13px] text-[#4A4A4A] font-Pretendard 
-                                ${item.id >= loginSupportData.length ? "" : "border-r-[1px] border-r-[#4a4a4a]"}
+      <div className="mt-[13px] w-full h-[19.5px] flex justify-center items-center">
+        {loginSupportData.map((item) => {
+          return (
+            <div key={item.id}>
+              {item.id === 3 ? (
+                <Link
+                  className={`px-[7px] text-[13px] text-[#4A4A4A] font-Pretendard 
+                                ${
+                                  item.id >= loginSupportData.length
+                                    ? ""
+                                    : "border-r-[1px] border-r-[#4a4a4a]"
+                                }
                                 
                                 `}
-                                    href={item.url}
-                                >
-                                    {item.title}
-                                </Link>
-                            ) : (
-                                <div
-                                    className={`px-[7px] text-[13px] text-[#4A4A4A] font-Pretendard 
-                            ${item.id >= loginSupportData.length ? "" : "border-r-[1px] border-r-[#4a4a4a]"}
+                  href={item.url}
+                >
+                  {item.title}
+                </Link>
+              ) : (
+                <div
+                  className={`px-[7px] text-[13px] text-[#4A4A4A] font-Pretendard 
+                            ${
+                              item.id >= loginSupportData.length
+                                ? ""
+                                : "border-r-[1px] border-r-[#4a4a4a]"
+                            }
                             
                             `}
-                                >
-                                    {item.title}
-                                </div>
-                            )}
-                        </div>
-                    );
-                })}
+                >
+                  {item.title}
+                </div>
+              )}
             </div>
+          );
+        })}
+      </div>
 
-            <ul className="w-full my-[40px] flex justify-around items-center">
-                {snsLoginData.map((sns: SnsLogin) => {
-                    return <SnsButton key={sns.id} snsType={sns.type} snsName={sns.name} iconPosition={sns.img} />;
-                })}
-            </ul>
+      <ul className="w-full my-[40px] flex justify-around items-center">
+        {snsLoginData.map((sns: SnsLogin) => {
+          return (
+            <SnsButton
+              key={sns.id}
+              snsType={sns.type}
+              snsName={sns.name}
+              iconPosition={sns.img}
+            />
+          );
+        })}
+      </ul>
 
-            <div className="mt-[100px]">
-                <Link href="">
-                    <button className="base-btn bg-[#222222] text-white text-[14px] font-semibold mb-[34px]">휴대폰 간편 로그인</button>
-                </Link>
+      <div className="mt-[100px]">
+        <button
+          onClick={() => alert("서비스 준비중입니다.")}
+          className="base-btn bg-[#222222] text-white text-[14px] font-semibold mb-[34px]"
+        >
+          휴대폰 간편 로그인
+        </button>
 
-                <Link href="/nonMemberLogin">
-                    <button className="base-btn bg-[#f3f3f3] text-[#9B9B9B] text-[13px]">비회원 조회하기</button>
-                </Link>
-            </div>
-        </div>
-    );
+        <button
+          onClick={() => alert("서비스 준비중입니다.")}
+          className="base-btn bg-[#f3f3f3] text-[#9B9B9B] text-[13px]"
+        >
+          비회원 조회하기
+        </button>
+      </div>
+    </div>
+  );
 }
